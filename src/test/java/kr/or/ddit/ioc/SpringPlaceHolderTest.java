62758bdd7cd95f6b8dba6ba5413a57bef1501b78
yohan <juenforever@naver.com>
2019-06-20 12:34:35 +0900

    spring ioc placeholder

---
+++ src/test/java/kr/or/ddit/ioc/SpringPlaceHolderTest.java
@@ -0,0 +1,35 @@
+package kr.or.ddit.ioc;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kr.or.ddit.ioc.placeholder.DbInfo;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:kr/or/ddit/ioc/application-ioc-placeholder.xml")
+public class SpringPlaceHolderTest {
+
+	@Resource(name = "dbInfo")
+	private DbInfo dbInfo;
+
+	@Test
+	public void dbInfoTest() {
+		/*** Given ***/
+
+		/*** When ***/
+		assertNotNull(dbInfo);
+		assertEquals("pc01", dbInfo.getUsername());
+		assertEquals("java", dbInfo.getPassword());
+
+		/*** Then ***/
+	}
+
+}
